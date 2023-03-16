72b1bf2470b6e241512d61d84ef085a12e52422b
Punyaphat Sukcharoenchaikul <pun.sukcharoenchaikul@mail.utoronto.ca>
2023-03-16 19:20:05 -0400

    initial commit util.py for getting img to np

---
+++ data_util/util.py
@@ -0,0 +1,40 @@
+"""
+This script converts the jpg images to numpy arrays and return it
+"""
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+def jpg_to_np(path):
+    """
+    This function converts the jpg images to numpy arrays
+    :param path: the path to the image
+    :return: the numpy array of the image
+    """
+    img = Image.open(path)
+    img = np.array(img)
+    return img
+
+
+def get_one_cut(path):
+    """
+    This function gets one cut of the video and return a list of numpy array and the text file
+    :param path: the path to the folder containing a jpg image and a text file
+    :return: the numpy array of the video and the text file
+    """
+    folder = os.listdir(path)
+    list_of_frames = []
+    # iterate through all files in the folder
+    for file in folder:
+        if file.endswith(".jpg"):
+            img = jpg_to_np(path + "/" + file)
+            list_of_frames.append(img)
+        elif file.endswith(".txt"):
+            # found a text file
+            text = open(path + "/" + file, "r")
+            text = text.read()
+
+    return list_of_frames, text
