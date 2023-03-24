3c6b7c958aed0057489464808da1e6d4d59c94d4
Punyaphat Sukcharoenchaikul <pun.sukcharoenchaikul@mail.utoronto.ca>
2023-03-24 15:59:26 -0400

    remove out_of_sight

--- data/generate_timestamps.py
+++ data/generate_timestamps.py
@@ -4,15 +4,12 @@ if __name__ == "__main__":
     # note: higher threshold means that the video will be split into more parts
     threshold = 0.8
     print("Threshold: ", threshold)
-    path = "/Users/punyaphatsuk/Documents/ECE324Data/Out of Sight/videos/out_of_sight_1"
-    list_of_frames, text = util.get_one_cut(path)
-    print(len(list_of_frames))
     timestamps = util.get_cut_timestamps(
-        "/Users/punyaphatsuk/Documents/ECE324Data/Out of Sight/out_of_sight.mp4", threshold)
+        "/Users/punyaphatsuk/Documents/ECE324Data/avatar-the-last-airbender-book-1-water-e02-the-a.mp4", threshold)
     print("timestamps: ", timestamps)
     print("len(timestamps): ", len(timestamps))
     timestamps = util.remove_consecutive_timestamps(timestamps)
     print("timestamps: ", timestamps)
     print("len(timestamps): ", len(timestamps))
     util.save_list_to_file(
-        timestamps, "/Users/punyaphatsuk/MIRAI-Future-Frame-Prediction-of-Anime/data/dataset/out_of_sight_timestamps.txt")
+        timestamps, "/Users/punyaphatsuk/MIRAI-Future-Frame-Prediction-of-Anime/data/dataset/avatar12.txt")
--- data/split_video.py
+++ data/split_video.py
@@ -2,9 +2,9 @@ import util
 import ast
 
 if __name__ == "__main__":
-    with open("/Users/punyaphatsuk/MIRAI-Future-Frame-Prediction-of-Anime/data/dataset/out_of_sight_timestamps.txt", "r") as f:
+    with open("/Users/punyaphatsuk/MIRAI-Future-Frame-Prediction-of-Anime/data/dataset/avatar12.txt", "r") as f:
         text = f.read()
     timestamps = ast.literal_eval(text)
-    output_path = "/Users/punyaphatsuk/Documents/ECE324Data/Out of Sight/auto_images/"
+    output_path = "/Users/punyaphatsuk/Documents/ECE324Data/avatar2/"
     util.split_to_images(
-        "/Users/punyaphatsuk/Documents/ECE324Data/Out of Sight/out_of_sight.mp4", output_path, timestamps)
+        "/Users/punyaphatsuk/Documents/ECE324Data/avatar-the-last-airbender-book-1-water-e02-the-a.mp4", output_path, timestamps)
